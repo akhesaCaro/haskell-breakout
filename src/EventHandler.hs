f7eb4ebc3c3ca74df6ca8b92b6f647c67584fcc3
akhesaCaro <caroline.gaudreau@gmail.com>
2018-03-04 20:22:15 +0100

    simple keyHandler for paddle

--- src/EventHandler.hs
+++ src/EventHandler.hs
@@ -10,5 +10,19 @@ module EventHandler
   import Graphics.Gloss.Interface.Pure.Game
   import System.Exit
 
+  -- | Pure responding to key events.
+  handleKeys :: Event     -- ^ keyEvent
+              -> Game      -- ^ Initial game state
+              -> Game      -- ^ Game updated
+  -- For an 'Left' or 'Right' keypress, move verticaly player1 paddle
+  handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) game =
+          game { paddlePos = (x - 10, y) }
+          where
+            (x, y) = paddlePos game
+  handleKeys (EventKey (SpecialKey KeyRight) Down _ _) game =
+          game { paddlePos = (x + 10, y) }
+          where
+            (x, y) = paddlePos game
+
   -- Do nothing for all other events.
   handleKeys _ game = game
